246e138dd724b00252f954268681fdaca2b327ad
James-Rouse <woootlooops@gmail.com>
2021-05-27 19:25:57 -0400

    Add clean_date function

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
 from sqlalchemy import create_engine, Column, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import datetime
+import csv
 
+
+def clean_date(date_string):
+    split_date = date_string.split("/")
+    year = int(split_date[2])
+    month = int(split_date[0])
+    day = int(split_date[1])
+    cleaned_date = datetime.date(year, month, day)
+    return cleaned_date
+
+
+def add_csv():
+    with open("inventory.csv") as csvfile:
+        inventory_reader = csv.reader(csvfile)
+        next(inventory_reader, None)
+        # Source for above line:
+        # https://stackoverflow.com
+        # /questions/14257373/skip-the-headers-when-editing-a-csv-file-using-python
+        for item in inventory_reader:
+            name = item[0]
+            price = item[1]
+            quantity = item[2]
+            date = clean_date(item[3])
 
 engine = create_engine("sqlite:///inventory.db", echo=False)
 Session = sessionmaker(bind=engine)
@@ -16,14 +40,18 @@ class Product(Base):
 
     product_id = Column(Integer, primary_key=True)
     product_name = Column(String)
-    product_quantity = Column(Integer)
     product_price = Column(Integer)
+    product_quantity = Column(Integer)
     date_updated = Column(Date)
 
     def __repr__(self):
-        return f"<Product(product_name={self.product_name}, product_quantity={self.product_quantity}, product_price={self.product_price}, date_updated={self.date_updated})>"
+        return f"<Product(product_name={self.product_name},\
+                product_quantity={self.product_quantity},\
+                product_price={self.product_price},\
+                date_updated={self.date_updated})>"
 
 
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
-    
+
+    add_csv()
