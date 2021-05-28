02c667986083619293db2e60115314025caf82fd
James-Rouse <woootlooops@gmail.com>
2021-05-28 12:59:41 -0400

    CSV contents added to DB
    -Function clean_price added.
    -DB file deletion added.

--- app.py
+++ app.py
@@ -3,34 +3,37 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
 import csv
+import os
 
 
-def clean_date(date_string):
-    split_date = date_string.split("/")
-    year = int(split_date[2])
-    month = int(split_date[0])
-    day = int(split_date[1])
-    cleaned_date = datetime.date(year, month, day)
-    return cleaned_date
-
-
-def add_csv():
-    with open("inventory.csv") as csvfile:
-        inventory_reader = csv.reader(csvfile)
-        next(inventory_reader, None)
-        # Source for above line:
-        # https://stackoverflow.com
-        # /questions/14257373/skip-the-headers-when-editing-a-csv-file-using-python
-        for item in inventory_reader:
-            name = item[0]
-            price = item[1]
-            quantity = item[2]
-            date = clean_date(item[3])
+try:
+    os.remove("inventory.db")
+    # ^^^Line 37 source: https://www.w3schools.com/python/python_file_remove.asp
+except PermissionError:
+    print(f"{'-'*107}\nPlease close any program accessing inventory.db so that \
+it can be deleted and remade when this script runs.\n{'-'*107}")
+except FileNotFoundError:
+    pass
 
 engine = create_engine("sqlite:///inventory.db", echo=False)
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
+
+
+def clean_date(date_string):
+    """Format date from csv appropriately for database entry."""
+    split_date = date_string.split("/")
+    year = int(split_date[2])
+    month = int(split_date[0])
+    day = int(split_date[1])
+    return datetime.date(year, month, day)
+
+
+def clean_price(uncleaned_price):
+    """Format price from csv appropriately for database entry."""
+    split_price = uncleaned_price.split("$")
+    return split_price[1]
 
 
 class Product(Base):
@@ -54,4 +57,18 @@ class Product(Base):
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
-    add_csv()
+    with open("inventory.csv") as csvfile:
+        inventory_reader = csv.reader(csvfile)
+        next(inventory_reader, None)
+        # ^^^Line 76 source:
+        # https://stackoverflow.com
+        # /questions/14257373/skip-the-headers-when-editing-a-csv-file-using-python
+        for item in inventory_reader:
+            name = item[0]
+            price = clean_price(item[1])
+            quantity = item[2]
+            date = clean_date(item[3])
+            product_one = Product(product_name=name, product_price=price,
+                                  product_quantity=quantity, date_updated=date)
+            session.add(product_one)
+            session.commit()
