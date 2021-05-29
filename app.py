6929411ddd00076718d6f63277984ae8a464a4f8
James-Rouse <woootlooops@gmail.com>
2021-05-29 12:46:27 -0400

    Menu progress ("A" option finished)
    -Finished add_product_to_db function.
    -Also added exit function to DB removal code.

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ try:
 except PermissionError:
     print(f"{'-'*107}\nPlease close any program accessing inventory.db so that \
 it can be deleted and remade when this script runs.\n{'-'*107}")
+    exit()
 except FileNotFoundError:
     pass
 
@@ -92,7 +93,7 @@ def menu():
     while True:
         print(f"""\n{'-'*15}\nStore Inventory\n{'-'*15}\n
             \r>Press [V] to view a specific inventory product's information.\n
-            \r>Press [A] to add a new product to the inventory.\n
+            \r>Press [A] to update or add a  product to the inventory.\n
             \r>Press [B] to generate a CSV backup of the store inventory.\n
             \r>Press [Q] to quit program.\n""")
         while True:
@@ -101,7 +102,7 @@ def menu():
             if answer == "V":
                 display_product_id()
             elif answer == "A":
-                pass
+                add_product_to_db()
             elif answer == "B":
                 pass
             elif answer == "Q":
@@ -130,8 +131,42 @@ def display_product_id():
 
 
 def add_product_to_db():
-    """Add new product to DB."""
-    pass
+    """Update or add product to DB."""
+    name = input("\nEnter new product's name: ")
+    while True:
+        try:
+            quantity = int(input("\nEnter new product's quantity: "))
+            break
+        except ValueError:
+            print("\nPlease enter an integer.")
+            continue
+    while True:
+        try:
+            price = int(input("\nEnter new product's price: "))
+            break
+        except ValueError:
+            print("\nPlease enter price as a total cents integer.")
+            continue
+    date = datetime.date.today()
+    new_product = Product(product_name=name, product_price=price,
+                          product_quantity=quantity, date_updated=date)
+    update_indicator = 0
+    for product in session.query(Product):
+        if product.product_name == name:
+            product.product_quantity = quantity
+            product.product_price = price
+            product.date_updated = date
+            session.commit()
+            update_indicator = 1
+            print(f"\n{name} has been updated:\n\
+                  {session.query(Product).get(product.product_id)}")
+            input("Enter any key to return to main menu: ")
+    if update_indicator == 0:
+        session.add(new_product)
+        session.commit()
+        print(f"\n{name} has been added to the inventory:\n\
+              {session.query(Product).get(new_product.product_id)}")
+        input("Enter any key to return to main menu: ")
 
 
 def backup_db_to_csv():
